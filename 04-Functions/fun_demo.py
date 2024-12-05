393172fa246a5ecdd9d8cf43058e0f18ca0b457c
Edgar <edgar.muoni@gmail.com>
2024-12-05 18:01:45 +0200

    Demonstrate correct PEP8 codestyle

---
+++ 04-Functions/fun_demo.py
@@ -0,0 +1,18 @@
+def triangle_area(a, h):
+    return a * h / 2
+
+
+s = triangle_area(10, 10)
+print(s)
+
+
+def hello():
+    print("Hello World")
+
+
+def hello_name(name):
+    print(f"Hello {name}")
+
+
+hello()
+hello_name("World")
